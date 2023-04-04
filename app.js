9bada0d59dd36c10268ff66bfc110059344cf3c3
Joseph Evangelista <oootep123@gmail.com>
2023-04-03 23:32:55 -0700

    implemented add button, counter and remove task button

--- app.js
+++ app.js
@@ -59,32 +59,16 @@ function createTask(task) {
     }
 
     const taskElMarkup = `
-    <div>
-                                <input type="checkbox"
+                             <div>
+                                    <input type="checkbox"
                                        name="tasks"
                                        id="${task.id}" ${task.isCompleted ? "checked" : ""}>
                                 <span ${!task.isCompleted ? "contenteditable" : ""}>${task.name}</span>
                             </div>
                             <button class="remove-task"
                                     title="Remove the task "${task.name}" task">
-                                <svg xmlns="http://www.w3.org/2000/svg"
-                                     class="icon icon-tabler icon-tabler-x"
-                                     width="24"
-                                     height="24"
-                                     viewBox="0 0 24 24"
-                                     stroke-width="2"
-                                     stroke="currentColor"
-                                     fill="none"
-                                     stroke-linecap="round"
-                                     stroke-linejoin="round">
-                                    <path stroke="none"
-                                          d="M0 0h24v24H0z"
-                                          fill="none"></path>
-                                    <path d="M18 6l-12 12"></path>
-                                    <path d="M6 6l12 12"></path>
-                                </svg>
+                                    &#10005
                             </button>
-    </div>
     `
 
     taskEl.innerHTML = taskElMarkup;
